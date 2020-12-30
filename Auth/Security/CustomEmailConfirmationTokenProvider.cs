3f7c58af48819afa4dcae59dda8702a6c8f83f03
ramin <ramin@RAMIN-PC>
2020-12-30 08:50:47 +0330

    ..

---
+++ Auth/Security/CustomEmailConfirmationTokenProvider.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Auth.Security
+{
+    public class CustomEmailConfirmationTokenProvider<TUser> : DataProtectorTokenProvider<TUser> where TUser : class
+    {
+        public CustomEmailConfirmationTokenProvider(IDataProtectionProvider dataProtectionProvider,
+                                        IOptions<CustomEmailConfirmationTokenProviderOptions> options,
+                                        ILogger<CustomEmailConfirmationTokenProvider<TUser>> logger)
+            : base(dataProtectionProvider, options, logger)
+        { }
+    }
+}
---
+++ Auth/Security/CustomEmailConfirmationTokenProviderOptions.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Auth.Security
+{
+    public class CustomEmailConfirmationTokenProviderOptions : DataProtectionTokenProviderOptions
+    { }
+}
